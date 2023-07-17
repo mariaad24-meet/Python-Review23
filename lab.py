16cddc5400d6603b719ea3cf8c363bdbd72eb864
mariaad24-meet <139689003+mariaad24-meet@users.noreply.github.com>
2023-07-17 18:04:19 +0300

    python lab

--- lab.py
+++ lab.py
@@ -4,20 +4,21 @@ def create_youtube_video (title ,description):
 	return d1 
 #3
 def like (youtube_video):
-	 if "likes" in youtube_video:
+	if "likes" in youtube_video:
 	 	youtube_video["likes"]+=1
-     return youtube_video
+	return youtube_video
 
 #4
 def dislike (youtube_video):
-	 if "dislikes" in youtube_video1:
+	if "dislikes" in youtube_video1:
 	 	youtube_video["dislikes"]+=1
-     return youtube_video
+	return youtube_video
 
 #5
-def add_comment(youtubevideo,username,comment_text):
+def add_comment(youtube_video,username,comment_text):
 	youtube_video["comments"][username]=comment_text
 	return youtube_video
 
 #6
-  ved=("hello","hello again")
+ved=create_youtube_video("hello","hello again")
+print(ved)
